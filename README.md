2efdba54e1df98d03070d2423fd70e237d406fe1
nggalien <nggalien.mail@gmail.com>
2020-01-22 10:37:09 +0100

    Initial commit
    First commit of the repo.

---
+++ README.md
@@ -0,0 +1,26 @@
+# upyHome nodes for node-red
+
+>In progress...
+
+The main purpose of **upyhome** is for home automation.
+
+This node-red package gives some nodes for interacting with Web REPL on a Micropython device.  
+You may use python classes and functions in the upyhome repository.    
+At this early stage of development you can just control:
+
+* digital inputs / outputs
+* files read / write
+
+Analog I/O, and drivers are planned.
+
+## upyHome client Node
+
+It uses standard a websocket client, derived from the official node-red ws client.  
+It support automatic reconnection
+
+## upyHome digital input
+
+## upyHome digital output
+
+
+
---
+++ uph-do/uph-do.js
@@ -0,0 +1,50 @@
+module.exports = function(RED) {
+    "use strict";
+
+    function UpyhomeDONode(config) {
+
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.clientConfig = RED.nodes.getNode(config.client);
+        this.number = config.number;
+        if (this.clientConfig) {
+            this.clientConfig.registerComponentNode("do", this.number, this);
+            // TODO: nls
+            this.clientConfig.on('opened', function(event) {
+                node.status({
+                    fill:"green",shape:"dot",text:RED._("websocket.status.connected",{count:event.count}),
+                    event:"connect",
+                    _session: {type:"websocket",id:event.id}
+                });
+            });
+            this.clientConfig.on('erro', function(event) {
+                node.status({
+                    fill:"red",shape:"ring",text:"common.status.error",
+                    event:"error",
+                    _session: {type:"websocket",id:event.id}
+                });
+            });
+            this.clientConfig.on('closed', function(event) {
+                var status;
+                if (event.count > 0) {
+                    status = {fill:"green",shape:"dot",text:RED._("websocket.status.connected",{count:event.count})};
+                } else {
+                    status = {fill:"red",shape:"ring",text:"common.status.disconnected"};
+                }
+                status.event = "disconnect";
+                status._session = {type:"websocket",id:event.id}
+                node.status(status);
+            });
+        } else {
+            this.error(RED._("websocket.errors.missing-conf"));
+        }
+        this.on('close', function() {
+            if (node.clientConfig) {
+                node.clientConfig.unregisterComponentNode("do", node.number, node);
+            }
+            node.status({});
+        });
+    }
+
+    RED.nodes.registerType("uph-do", UpyhomeDONode);
+}
